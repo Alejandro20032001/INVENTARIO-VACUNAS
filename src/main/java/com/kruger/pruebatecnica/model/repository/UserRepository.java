3b6c715895f4a29f7f80c7efeb41624a6c7adcf9
Alejandro20032001 <dilanantezama@gmail.com>
2023-03-16 18:42:07 -0400

    sql code for searching

--- src/main/java/com/kruger/pruebatecnica/model/repository/UserRepository.java
+++ src/main/java/com/kruger/pruebatecnica/model/repository/UserRepository.java
@@ -2,6 +2,44 @@ package com.kruger.pruebatecnica.model.repository;
 
 import com.kruger.pruebatecnica.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query(value = """
+            SELECT u.username,\s
+            ui.address,\s
+            ui.birth_date,\s
+            ui.email,\s
+            ui.last_name,\s
+            ui.name,\s
+            ui.phone,\s
+            v.vaccination_date,\s
+            va.name
+            FROM public.user u \s
+            LEFT JOIN user_information ui ON u.id_user_information = ui.id\s
+            LEFT JOIN vaccination v ON ui.id_vaccination = v.id\s
+            LEFT JOIN vaccine va ON v.id_vaccine = va.id\s
+            WHERE u.delete = false AND ui.delete = false AND v.delete = false
+            """, nativeQuery = true)
+    List findAll();
+    @Query(value = """
+            SELECT u.username,\s
+            ui.address,\s
+            ui.birth_date,\s
+            ui.email,\s
+            ui.last_name,\s
+            ui.name,\s
+            ui.phone,\s
+            v.vaccination_date,\s
+            va.name
+            FROM public.user u \s
+            LEFT JOIN user_information ui ON u.id_user_information = ui.id \s
+            LEFT JOIN vaccination v ON ui.id_vaccination = v.id \s
+            LEFT JOIN vaccine va ON v.id_vaccine = va.id \s
+            WHERE u.delete = false AND ui.delete = false AND v.delete = false
+            AND va.name = :vaccineName""", nativeQuery = true)
+    List findByVaccineName(@Param("vaccineName") String vaccineName);
 }
