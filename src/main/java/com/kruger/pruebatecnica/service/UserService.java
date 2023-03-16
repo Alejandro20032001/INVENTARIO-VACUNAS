e83968e76c74cec39f6304b2d5e0ff295fb235dd
Alejandro20032001 <dilanantezama@gmail.com>
2023-03-16 18:42:39 -0400

    Change on return types

--- src/main/java/com/kruger/pruebatecnica/service/UserService.java
+++ src/main/java/com/kruger/pruebatecnica/service/UserService.java
@@ -4,13 +4,14 @@ import com.kruger.pruebatecnica.model.entity.User;
 import com.kruger.pruebatecnica.model.pojo.dto.UserDTO;
 import com.kruger.pruebatecnica.model.pojo.vo.UserVO;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
     Optional<UserVO> findById(int id);
-    Optional<UserVO> findAllVaccinated();
-    Optional<UserVO> findAllNotVaccinated();
-    Optional<UserVO> findByVaccine(int idVaccine);
+    List<UserVO> findAllVaccinated();
+    List<UserVO> findAllNotVaccinated();
+    List<UserVO> findByVaccine(int idVaccine);
     UserVO persistUser(UserDTO userDTO);
     void deleteUser(int idUser);
     UserVO entityToVO (User user);
