df3dae220161a04974c9485ec6b7689fdf30026b
Alejandro20032001 <dilanantezama@gmail.com>
2023-03-16 00:09:31 -0400

    vos for entities

---
+++ src/main/java/com/kruger/pruebatecnica/model/pojo/vo/VaccinationVO.java
@@ -0,0 +1,14 @@
+package com.kruger.pruebatecnica.model.pojo.vo;
+
+import com.kruger.pruebatecnica.model.entity.Vaccine;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class VaccinationVO {
+    private Date vaccinationDate;
+    private Vaccine vaccine;
+}
