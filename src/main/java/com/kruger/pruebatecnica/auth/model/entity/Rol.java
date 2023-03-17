2b818ae2776206174d8c270d3c414f17227bce48
Alejandro20032001 <dilanantezama@gmail.com>
2023-03-17 16:21:55 -0400

    change on data type of rolname

---
+++ src/main/java/com/kruger/pruebatecnica/auth/model/entity/Rol.java
@@ -0,0 +1,19 @@
+package com.kruger.pruebatecnica.auth.model.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "rol", schema = "public")
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    private String rolName;
+}
