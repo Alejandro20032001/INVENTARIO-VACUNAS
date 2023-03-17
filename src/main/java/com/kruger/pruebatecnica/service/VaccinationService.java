7d80ff175011969b797736d3533f9720b74c104a
Alejandro20032001 <dilanantezama@gmail.com>
2023-03-16 21:54:13 -0400

    findByIdVo added on vaccination service

--- src/main/java/com/kruger/pruebatecnica/service/VaccinationService.java
+++ src/main/java/com/kruger/pruebatecnica/service/VaccinationService.java
@@ -1,12 +1,15 @@
 package com.kruger.pruebatecnica.service;
 
+import com.kruger.pruebatecnica.model.entity.Vaccination;
+import com.kruger.pruebatecnica.model.pojo.dto.VaccinationDTO;
 import com.kruger.pruebatecnica.model.pojo.vo.VaccinationVO;
 
 import java.util.Optional;
 
 public interface VaccinationService {
-    Optional<VaccinationVO> findById(int id);
-    VaccinationVO persistVaccination(VaccinationVO vaccinationVO);
+    Optional<VaccinationVO> findByIdVO(int id);
+    Optional<Vaccination> findById(int id);
+    VaccinationVO persistVaccination(VaccinationDTO vaccinationDTO);
     void deleteVaccination(int idVaccination);
-    VaccinationVO entityToVO (VaccinationVO vaccinationVO);
+    VaccinationVO entityToVO (Vaccination vaccination);
 }
