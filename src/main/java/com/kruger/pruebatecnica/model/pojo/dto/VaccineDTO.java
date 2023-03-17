e8fa15a1ec80bb97e005d4fe05451111fa08c38e
Alejandro20032001 <dilanantezama@gmail.com>
2023-03-16 23:58:34 -0400

    Validation for not null fields

--- src/main/java/com/kruger/pruebatecnica/model/pojo/dto/VaccineDTO.java
+++ src/main/java/com/kruger/pruebatecnica/model/pojo/dto/VaccineDTO.java
@@ -1,10 +1,13 @@
 package com.kruger.pruebatecnica.model.pojo.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+
 
 @Getter
 @Setter
 public class VaccineDTO {
+    @NotNull(message = "This field cannot be null")
     private String name;
 }
