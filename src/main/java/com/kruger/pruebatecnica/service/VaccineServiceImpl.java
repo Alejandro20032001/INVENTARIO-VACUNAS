3f01a0535b3df85790b33ab9e5e4001f97ac2c02
Alejandro20032001 <dilanantezama@gmail.com>
2023-03-16 21:40:29 -0400

    vaccine services implementation

---
+++ src/main/java/com/kruger/pruebatecnica/service/VaccineServiceImpl.java
@@ -0,0 +1,58 @@
+package com.kruger.pruebatecnica.service;
+
+import com.kruger.pruebatecnica.model.entity.Vaccine;
+import com.kruger.pruebatecnica.model.pojo.dto.VaccineDTO;
+import com.kruger.pruebatecnica.model.pojo.vo.VaccineVO;
+import com.kruger.pruebatecnica.model.repository.VaccineRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class VaccineServiceImpl implements VaccineService{
+    private final VaccineRepository vaccineRepository;
+    @Autowired
+    public VaccineServiceImpl(VaccineRepository vaccineRepository) {
+        this.vaccineRepository = vaccineRepository;
+    }
+
+    @Override
+    public Optional<VaccineVO> findByIdVO(int id) {
+        Vaccine vaccine = vaccineRepository.findById(id).get();
+        Optional optional = Optional.empty();
+        if(vaccine != null)
+            optional = Optional.of(entityToVO(vaccine));
+
+        return optional;
+    }
+    @Override
+    public Optional<Vaccine> findById(int id) {
+        Vaccine vaccine = vaccineRepository.findById(id).get();
+        Optional optional = Optional.empty();
+        if(vaccine != null)
+            optional = Optional.of(vaccine);
+
+        return optional;
+    }
+
+    @Override
+    public VaccineVO persistVaccine(VaccineDTO vaccineDTO) {
+        Vaccine vaccine = new Vaccine();
+        BeanUtils.copyProperties(vaccineDTO, vaccine);
+        return entityToVO(vaccineRepository.save(vaccine));
+    }
+
+    @Override
+    public void deleteVaccine(int idVaccine) {
+        vaccineRepository.deleteById(idVaccine);
+    }
+
+    @Override
+    public VaccineVO entityToVO(Vaccine vaccine) {
+        VaccineVO vaccineVO = new VaccineVO();
+        BeanUtils.copyProperties(vaccine, vaccineVO);
+        return vaccineVO;
+    }
+}
