43ef81c13d6ca6378e1399bb59a71cf154e15ad3
Alejandro20032001 <dilanantezama@gmail.com>
2023-03-18 15:54:36 -0400

    Delete of system.out

--- src/main/java/com/kruger/pruebatecnica/auth/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/kruger/pruebatecnica/auth/jwt/JwtAuthenticationFilter.java
@@ -21,7 +21,6 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             HttpServletResponse response)  {
         Credentials authCredentials =new Credentials();
         try {
-            //System.out.println("esta intentando");
             authCredentials = new ObjectMapper().readValue(
                     request.getReader(), Credentials.class
             );
@@ -34,8 +33,6 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                             authCredentials.getUsername(),
                             authCredentials.getContrasena(),
                             Collections.emptyList());
-            System.out.println("USERNAME: "+authCredentials.getUsername());
-            System.out.println("PASSWORD: "+authCredentials.getContrasena());
             return getAuthenticationManager().authenticate(PAToken);
         }catch (
                 AuthenticationException e){
@@ -52,10 +49,8 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             FilterChain filterChain,
             Authentication authentication
     ) throws IOException {
-        System.out.println("ENTRA AL SUCCESS ");
         UserDetailImpl userDetails= (UserDetailImpl) authentication.getPrincipal();
         String token = JwtTokenUtils.createToken(userDetails);
-        System.out.println("token: "+token);
         response.addHeader("Authorization", "Bearer "+ token);
         response.getWriter().flush();
     }
