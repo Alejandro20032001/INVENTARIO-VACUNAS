c8bd22e770a852251f2adf2af417c8caafb9490d
Alejandro20032001 <dilanantezama@gmail.com>
2023-03-17 22:40:37 -0400

    changes on services

--- src/main/java/com/kruger/pruebatecnica/service/UserInformationService.java
+++ src/main/java/com/kruger/pruebatecnica/service/UserInformationService.java
@@ -1,6 +1,8 @@
 package com.kruger.pruebatecnica.service;
 
 import com.kruger.pruebatecnica.model.entity.UserInformation;
+import com.kruger.pruebatecnica.model.pojo.dto.RegisterUserDTO;
+import com.kruger.pruebatecnica.model.pojo.dto.UpdateInformationDTO;
 import com.kruger.pruebatecnica.model.pojo.dto.UserInformationDTO;
 import com.kruger.pruebatecnica.model.pojo.vo.UserInformationVO;
 
@@ -9,8 +11,9 @@ import java.util.Optional;
 public interface UserInformationService {
     Optional<UserInformationVO> findByIdVO(int id);
     Optional<UserInformation> findById(int id);
-    UserInformationVO persistUserInformation(UserInformationDTO userInformationDTO);
+    UserInformation persistUserInformation(RegisterUserDTO registerUserDTO);
     void deleteUserInformation(int idUserInformation);
-    void updateUserInformation(int idUserInformation, UserInformationDTO userInformationDTO);
+    UserInformationVO updateUserInformation(int idUserInformation, UpdateInformationDTO updateInformationDTO);
+
     UserInformationVO entityToVO (UserInformation userInformation);
 }
