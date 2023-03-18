a18981a29669d7ccaf7f06e87cb29367e62c59bf
Alejandro20032001 <dilanantezama@gmail.com>
2023-03-18 15:57:17 -0400

    query deleted

--- src/main/java/com/kruger/pruebatecnica/auth/model/repository/UserRepository.java
+++ src/main/java/com/kruger/pruebatecnica/auth/model/repository/UserRepository.java
@@ -9,22 +9,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    @Query(value = """
-            SELECT u.username,\s
-            ui.address,\s
-            ui.birth_date,\s
-            ui.email,\s
-            ui.last_name,\s
-            ui.name,\s
-            ui.phone,\s
-            v.vaccination_date,\s
-            va.name
-            FROM public.user u \s
-            LEFT JOIN user_information ui ON u.id_user_information = ui.id\s
-            LEFT JOIN vaccination v ON ui.id_vaccination = v.id\s
-            LEFT JOIN vaccine va ON v.id_vaccine = va.id\s
-            WHERE u.delete = false AND ui.delete = false AND v.delete = false
-            """, nativeQuery = true)
     List findAll();
     @Query(value = """
             SELECT u.username,\s
@@ -43,11 +27,11 @@ public interface UserRepository extends JpaRepository<User, Integer> {
             WHERE u.delete = false AND ui.delete = false AND v.delete = false
             AND va.name = :vaccineName""", nativeQuery = true)
     List findByVaccineName(@Param("vaccineName") String vaccineName);
-    @Query(value = """
+    /*@Query(value = """
             SELECT u.*\s
             FROM public.user u \s
             WHERE u.delete = false AND
-            u.username = :username""", nativeQuery = true)
+            u.username = :username""", nativeQuery = true)*/
     Optional<User> findByUsername(@Param("username") String username);
     boolean existsByUsername(String username);
 }
